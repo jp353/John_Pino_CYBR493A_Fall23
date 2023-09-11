dcfdb2e1d824a719af8877e326efd1216ffad6cf
John <142448925+jp353@users.noreply.github.com>
2023-09-11 16:11:37 -0400

    I added a program that displays an example of an if statement

---
+++ In-class Activities/InClassActivity2.py
@@ -0,0 +1,18 @@
+"""
+John  Pino
+This is an example of an If statement in Python
+CYBR-493A-001
+9/11/23
+
+
+"""
+# Accept value
+value1 = input('Please input value 1')
+#Accept value
+value2 = input('Please input value 2')
+
+if value1 >= value2:
+    print(value1 + " is greater than or equal to " + value2)
+else:
+    print(value2 + " is greater than " + value1)
+
