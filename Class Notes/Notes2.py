081d795135f39e3b77d09cf8f16b0a560be894d0
John <142448925+jp353@users.noreply.github.com>
2023-09-27 16:23:52 -0400

    I created a program that allows a user to choose between 2 programs

---
+++ Class Notes/Notes2.py
@@ -0,0 +1,7 @@
+def main ():
+    print(validlength)
+
+def
+
+# You can make an if statemwnt without the else by just putting "return False" at the end
+
---
+++ In-class Activities/In_Class_Activity_5.py
@@ -0,0 +1,19 @@
+"""
+John Pino
+9/29/23
+In Class Activity 5
+
+This is a program that prompts the user to choose between 2 programs.
+It then runs the selected program.
+"""
+
+import InClassActivity2
+import InClassActivity3
+
+
+print("Hello, please pick an activity to run. ")
+print("You can choose In-Class Acttivity 2 or In-Class Activity 3")
+
+userChoice = input("Chosen Activity: ")
+userChoice = int(userChoice)
+
