93d197faab2c3041611604766aa699834b3d7d99
John <142448925+jp353@users.noreply.github.com>
2023-09-29 16:04:34 -0400

    Added an If/Else statement as an attempt to solve the import issue with In-Class 3.
    It did not work    :(

--- In-class Activities/In_Class_Activity_5.py
+++ In-class Activities/In_Class_Activity_5.py
@@ -7,13 +7,14 @@ This is a program that prompts the user to choose between 2 programs.
 It then runs the selected program.
 """
 
-import InClassActivity2
-import InClassActivity3
-
-
 print("Hello, please pick an activity to run. ")
 print("You can choose In-Class Acttivity 2 or In-Class Activity 3")
 
 userChoice = input("Chosen Activity: ")
-userChoice = int(userChoice)
+
+if userChoice == "2":
+    import InClassActivity2
+else:
+    import InClassActivity3
+
 
