9fe7d7730fabbac0603651c9f58c1693cdba0ecc
John <142448925+jp353@users.noreply.github.com>
2023-10-27 18:31:44 -0400

    Added current draft of Activity_8.py

--- In-class Activities/Activity_8.py
+++ In-class Activities/Activity_8.py
@@ -2,22 +2,20 @@ import requests
 from lxml import html
 import Web_Scraping
 
-url = "https://www.wvu.edu"  # Make sure to include the protocol (https://) in the URL
+url = "https://www.wvu.edu"
 
 response = requests.get(url)
 
 if response.status_code == 200:
-    # Parse the HTML content of the page using lxml
+
     tree = html.fromstring(response.content)
 
-    # Use your extract_text function from Web_Scraping.py to extract 5 different texts
+
     texts = extract_text(tree, 5)
 
-    # Print all the extracted text
     for text in texts:
         print(text)
 
-    # Commit and push your changes to your version control system (e.g., Git)
-    # This step depends on the version control system you are using
+
 else:
     print("Failed to retrieve the webpage. Status code:", response.status_code)
