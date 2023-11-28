1b569e62b69d80f37b529a4315566f4a729c76e0
John <142448925+jp353@users.noreply.github.com>
2023-11-27 20:50:19 -0500

    Making progress

--- In-class Activities/In_Class_Activity_14.py
+++ In-class Activities/In_Class_Activity_14.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import html
+import re
 
 def get_web_tree(link):
     page = requests.get(link)
@@ -10,6 +11,11 @@ link = "https://cwe.mitre.org/data/definitions/888.html"
 main_tree = get_web_tree(link)
 
 # Print the text content of each matching element
-primaries = main_tree.xpath('//*[@class="group"]')
+primaries = main_tree.xpath('//*[@class="group"]//text()')
 for primary in primaries:
-    print(primary.text_content().strip())
+    # Use regular expression to find text within parentheses
+    matches = re.findall(r'\((.*?)\)', primary)
+
+    # Print the matches
+    for match in matches:
+        print(match.strip())
