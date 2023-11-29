8c0da38372ce204798ee339b0867825ebdeaf7d6
John <142448925+jp353@users.noreply.github.com>
2023-11-29 16:04:19 -0500

    Still figuring out how to eliminate the primary and secondary clusters from the search

--- In-class Activities/In_Class_Activity_14.py
+++ In-class Activities/In_Class_Activity_14.py
@@ -12,22 +12,14 @@ main_tree = get_web_tree(link)
 
 
 primaries = main_tree.xpath('//*[@class="group"]//text()')
-primaries_raw = main_tree.xpath('//*[@class="group"]//text()')
-pattern = re.compile(r'(.+?) - \((\d{3})\)')
+pattern = re.compile(r'(\s*.+\s*)(\s*-\s* +\(\d{3}\))')
 
 
-for i in primaries:
-    print(i)
-
-
-'''for primary in primaries:
+for primary in primaries:
     # Find pattern
     match = pattern.search(primary)
 
     # Check if the pattern is found
     if match:
         # Print the matched components directly
-        print(
-            f"CWE: {match.group(1).strip()}\nThree-digit number: {match.group(2).strip()}")
-    else:
-        print("None found")'''
+        print(f"CWE Title: {match.group(1).strip()}\nThree-digit number: {match.group(2).strip()}")
